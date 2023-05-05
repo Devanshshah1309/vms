4574fb933a08344428518844330ffb6a04f4e2b7
DevanshShah1309 <devanshshah1309@gmail.com>
2023-05-05 18:41:14 +0800

    Allow different streams to be in full screen mode
    Also updated README

--- vms.py
+++ vms.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 from enum import Enum
 import utils
 import logging
@@ -22,6 +21,16 @@ class Options(Enum):
     FULLSCREEN = ord('f')
     REWIND = ord('a')
     FAST_FORWARD = ord('d')
+    FULLSCREEN_ORIGINAL = ord('1')
+    FULLSCREEN_GRAYSCALE = ord('2')
+    FULLSCREEN_BLUR = ord('3')
+    FULLSCREEN_EDGE_DETECT = ord('4')
+
+class FullScreenMode(Enum):
+    ORIGINAL = 0
+    GRAYSCALE = 1
+    BLUR = 2
+    EDGE_DETECT = 3
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -29,19 +38,36 @@ logging.basicConfig(level=logging.DEBUG)
 video_is_paused = False
 video_is_fullscreen = False
 video_is_ended = False
+fullscreen_mode = FullScreenMode.ORIGINAL
 
 def change_settings(key):
     # rewind and fast forward are handled in the main loop because they affect the video itself
     # not just the display
     if key == -1: # -1 => no key pressed
         return
-    global video_is_paused, video_is_fullscreen, video_is_ended
+    global video_is_paused, video_is_fullscreen, video_is_ended, fullscreen_mode
     if key == Options.PAUSE_OR_PLAY.value:
         logging.info("Toggling pause/play.")
         video_is_paused = not video_is_paused
     elif key == Options.FULLSCREEN.value:
         logging.info("Toggling fullscreen.")
         video_is_fullscreen = not video_is_fullscreen
+    elif key == Options.FULLSCREEN_ORIGINAL.value:
+        logging.info("Setting fullscreen mode to original.")
+        video_is_fullscreen = True
+        fullscreen_mode = FullScreenMode.ORIGINAL
+    elif key == Options.FULLSCREEN_GRAYSCALE.value:
+        logging.info("Setting fullscreen mode to grayscale.")
+        video_is_fullscreen = True
+        fullscreen_mode = FullScreenMode.GRAYSCALE
+    elif key == Options.FULLSCREEN_BLUR.value:
+        logging.info("Setting fullscreen mode to blur.")
+        video_is_fullscreen = True
+        fullscreen_mode = FullScreenMode.BLUR
+    elif key == Options.FULLSCREEN_EDGE_DETECT.value:
+        logging.info("Setting fullscreen mode to edge detect.")
+        video_is_fullscreen = True
+        fullscreen_mode = FullScreenMode.EDGE_DETECT
     elif key == Options.QUIT.value:
         logging.info("Ending video.")
         video_is_ended = True
@@ -58,6 +84,17 @@ def handle_pause_play():
     if key == Options.PAUSE_OR_PLAY.value:
         return True
     return handle_pause_play() # recursively call until valid key is pressed
+
+def handle_fullscreen_mode_image(img, fullscreen_mode):
+    if fullscreen_mode == FullScreenMode.ORIGINAL:
+        pass
+    elif fullscreen_mode == FullScreenMode.GRAYSCALE:
+        img = utils.grayscale(img)
+    elif fullscreen_mode == FullScreenMode.BLUR:
+        img = utils.blur(img, DEFAULT_BLUR_KERNEL_SIZE)
+    elif fullscreen_mode == FullScreenMode.EDGE_DETECT:
+        img = utils.edge_detect(img, DEFAULT_CANNY_MIN_THRESHOLD, DEFAULT_CANNY_MAX_THRESHOLD)
+    return utils.get_fullscreen_image(img, DEFAULT_GRID_COLS, DEFAULT_GRID_ROWS)
 
 def get_frames_offset(fps):
     return fps * DEFAULT_REWIND_FAST_FORWARD_SECONDS
@@ -80,7 +117,7 @@ def process_video(video_path = DEFAULT_VIDEO_PATH):
             print("End of Video!")
             break
         if video_is_fullscreen:
-            display_image = utils.get_fullscreen_image(img, DEFAULT_GRID_COLS, DEFAULT_GRID_ROWS)
+            display_image = handle_fullscreen_mode_image(img, fullscreen_mode)
         else:
             display_image = utils.get_gray_blur_edgedetect_image(img, DEFAULT_BLUR_KERNEL_SIZE,
                                 DEFAULT_CANNY_MIN_THRESHOLD, DEFAULT_CANNY_MAX_THRESHOLD)
